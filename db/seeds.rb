b8ba49a46e6498613ee5da01e25347bd743c937d
Björn Grunde <bjorn@imagineit.se>
2017-02-28 15:34:00 +0100

    added better seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,16 @@
 # Should be extracted to its own seed file later on
 
 
+#Create 12 users, one of each proffession(World of Warcraft class, not to be confused with a Class)
 User::CreateUserFromGuildApplication.(user: { username: "Sprayandpray", email: "bjorngrunde@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "hunter", rank: 1, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Dubbelmums", email: "dubbelmums@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "warlock", rank: 1, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Bombka", email: "bombka@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "warrior", rank: 1, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Nightshade", email: "nightshade@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "druid", rank: 1, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Stickado", email: "stickado@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "rogue", rank: 1, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "burnmtfkrs", email: "burnmtfkrs@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "mage", rank: 1, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Rigormoris", email: "rigor@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "priest", rank: 3, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Izhrak", email: "izhrak@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "death_knight", rank: 3, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Litenmenvass", email: "litenmenvass@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "monk", rank: 3, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Bäverbonkarn", email: "baverbonkarn@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "shaman", rank: 3, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Bubbleoncd", email: "bubbleoncd@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "paladin", rank: 3, server: "grim batol", spec: "Beast Mastery"} })
+User::CreateUserFromGuildApplication.(user: { username: "Demonhoney", email: "bubbleoncd@live.se", profile: { first_name: "Björn", last_name: "Grunde", klass: "demon_hunter", rank: 3, server: "grim batol", spec: "Beast Mastery"} })
