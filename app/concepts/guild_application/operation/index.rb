b79b340e38126c401e4a8db6530f710b6b0bb440
bjorngrunde <bjorngrunde@live.se>
2017-03-11 19:39:31 +0100

    added search fields to GA & Users

--- app/concepts/guild_application/operation/index.rb
+++ app/concepts/guild_application/operation/index.rb
@@ -7,6 +7,7 @@ class GuildApplication::Index < Trailblazer::Operation
 
   def get_all_or_pending!(options, params:, **)
     return options["model"] = GuildApplication.is_pending if params[:status]
-    options["model"] = GuildApplication.all
+    return options["model"] = GuildApplication.order(:first_name).page(options["params"]["page"]).per(15) unless options["params"]["term"]
+    options["model"] = GuildApplication.where("first_name LIKE :term OR last_name LIKE :term", term: "%#{options["params"]["term"]}%" ).page(options["params"]["page"])
   end
 end
