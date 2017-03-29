4901dbc135bfdfbf761075eb76624df7c5728a7c
Björn Grunde <dragonslayer@Bjorns-iMac.local>
2017-03-29 23:57:35 +0200

    added stuff to policy

--- app/concepts/user/operation/create_user_from_guild_application.rb
+++ app/concepts/user/operation/create_user_from_guild_application.rb
@@ -8,7 +8,7 @@ class User::CreateUserFromGuildApplication < Trailblazer::Operation
 		{ "name" => mutable_data["contract.default"].username,
 			"realm" => mutable_data["contract.default"].profile.server,
 			"contract" => mutable_data["contract.default"] }
-	end 
+	end
 	)
 	step 	:add_thumbnail!
 	step Nested(:update_meta_data!, input: ->(options, mutable_data: , runtime_data:, **)do
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,11 +1,11 @@
 class UserPolicy
-  
+
   def initialize(user, model)
     @user, @model = user, model
   end
 
   def create?
-    @model.id.nil?
+    @model.id.nil? && @model.creatable_by?(@user)
   end
 
   def update?
