17fd346244e8257fbf1e8550a61894358ddb1b33
Björn Grunde <bjorn@imagineit.se>
2017-03-31 10:34:42 +0200

    added role test

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create several roles for a user" do
+    attrs = attributes_for :user
+    
+    user = User.create(attrs)
+    
+    user.add_role(:member)
+    user.add_role(:moderator)
+    user.add_role(:developer)
+    
+    expect(user.has_role? :developer).to be_true
+    expect(user.has_role? :moderator).to be_true
+    expect(user.has_role? :member).to be_true
+  end
 end
