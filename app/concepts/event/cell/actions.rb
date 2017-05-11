c7ca9b19a1d4b8abea56041afb8f01496804f073
bjorngrunde <bjorngrunde@live.se>
2017-05-11 23:58:18 +0200

    it works..it..oh wait..no..gawd nooo

--- app/concepts/event/cell/actions.rb
+++ app/concepts/event/cell/actions.rb
@@ -2,7 +2,7 @@ module Event::Cell
   class Actions < Event::Cell::Master
 
     def select_players
-      model.invites.map { |invite| [invite.user.username.humanize, invite.id] unless invite.status == "selected" }
+      model.invites.map { |invite| [invite.user.username.humanize, invite.id] unless invite.status == "selected" || invite.role.nil? }
     end
   end
 end
--- app/concepts/event/operation/index.rb
+++ app/concepts/event/operation/index.rb
@@ -3,6 +3,6 @@ class Event::Index < Trailblazer::Operation
   step :find_all!
 
   def find_all!(options, params:, **)
-    options["model"] = Event.page(params[:page]).per(16)
+    options["model"] = Event.today_and_forward.includes(:invites).where("invites.user_id = ?", options["current_user"].id).references(:invites).page(params[:page]).per(16)
   end
 end
