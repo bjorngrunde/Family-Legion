cbb700e0aa2e0b0e9b33dd8cc209a45abebacedd
bjorngrunde <bjorngrunde@live.se>
2017-08-20 13:51:30 +0200

    Comments no load 20 at a time

--- app/concepts/comment/cell/show.rb
+++ app/concepts/comment/cell/show.rb
@@ -7,7 +7,7 @@ module Comment::Cell
     end
 
     def comments
-      @comments ||= model.comments.order(created_at: :desc).page(page).per(5)
+      @comments ||= model.comments.order(created_at: :desc).page(page).per(20)
     end
 
     def page
